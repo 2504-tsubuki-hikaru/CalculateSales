bebe3579c903b32514f4c7768fcd6453ce6a1a01
tsubuki_hikaru <tsubuki.hikaru@alhinc.jp>
2025-04-09 16:46:22 +0900

    最新

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CalculateSales {
@@ -37,15 +39,36 @@ public class CalculateSales {
 		}
 
 		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
+		File[] files = new File(args[0]).listFiles();
+
+		List<File> rcdFiles = new ArrayList<>();
+
+		for(int i = 0; i < files.length ;  i++) {
+
+			if(files[i].getName().matches("^[0-9]{8}.rcd$")) {
+				rcdFiles.add(files[i]);
+
+			}
+		}
+
+		for(int i = 0; i < rcdFiles.size(); i++) {
+			/*long fileSale = Long.parseLong(売上⾦額);
+			Long saleAmount = 売上⾦額を⼊れたMap.get(⽀店コード) + long に変換した売上⾦額;
+*/
+		}
+
+
+
+
 
 
 
 		// 支店別集計ファイル書き込み処理
 		if(!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)) {
-			return;
-		}
+		return;
 
-	}
+		}
+}
 
 	/**
 	 * 支店定義ファイル読み込み処理
@@ -67,13 +90,11 @@ public class CalculateSales {
 			String line;
 			// 一行ずつ読み込む
 			while((line = br.readLine()) != null) {
-				// ※ここの読み込み処理を変更してください。(処理内容1-2)
+				// ※ここの読み込み処理を変更してください。(処理内容1-2)完了済み
 				String[] items  = line.split(",");
 
 				branchNames.put(items[0], items[1] );
 				branchSales.put(items[0],  0L);
-
-				
 
 			}
 	}catch(IOException e){
